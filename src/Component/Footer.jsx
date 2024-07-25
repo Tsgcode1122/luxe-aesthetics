f179abdbb64b31861bdab89cca8c7f1d9fa9411a
Tsgcode1122 <Tsgcode1122@users.noreply.github.com>
2024-07-25 10:33:45 +0100

    footer

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import styled from "styled-components";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Container>
+      <Content>
+        <p>© 2024 Gem Luxe Aesthetics. All rights reserved.</p>
+      </Content>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  width: 100%;
+  padding: 1rem 0;
+  background: linear-gradient(
+    190deg,
+    #1f1809 10%,
+    #181501 30%,
+    #1f1809 50%,
+    #181501 70%,
+    #2f240d 90%,
+    #181501 100%
+  );
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  transition: top 0.3s;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Content = styled.div`
+  text-align: center;
+  color: white;
+  font-size: 0.9rem;
+  font-family: "Arial", sans-serif;
+
+  p {
+    margin: 0;
+  }
+`;
 
 export default Footer;
--- src/Component/Layout.jsx
+++ src/Component/Layout.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import { Outlet, useLocation } from "react-router-dom";
 
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const Layout = () => {
   return (
     <>
       <Navbar />
       <Outlet />
+      <Footer />
     </>
   );
 };
