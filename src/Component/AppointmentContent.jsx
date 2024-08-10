024cf7e8d3d2dddb65492418d8f7abedaa2b9c30
Tsgcode1122 <Tsgcode1122@users.noreply.github.com>
2024-08-11 00:08:01 +0100

    s

--- src/Component/AppointmentContent.jsx
+++ src/Component/AppointmentContent.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import { Skeleton } from "antd"; // Import the Skeleton component from Ant Design
+import { Skeleton } from "antd";
 
 const AppointmentContent = () => {
   const [loading, setLoading] = useState(true);
@@ -8,13 +8,14 @@ const AppointmentContent = () => {
   const handleLoad = () => {
     setLoading(false);
   };
-
   return (
     <>
       {/* <Content>
-        Book your personalized weight loss appointment with our expert team.
-        We're here to provide the best care and tailored treatments to help you
-        achieve your wellness goals. Let's get started!
+        We're excited to support you on your weight loss journey. Use the form
+        below to book your personalized appointment. Our expert team is
+        dedicated to providing you with the best care and effective treatments
+        tailored just for you. Let's get started on achieving your wellness
+        goals!
       </Content> */}
       <Container>
         {loading && (
@@ -35,7 +36,32 @@ const AppointmentContent = () => {
 };
 
 export default AppointmentContent;
+const Content = styled.p`
+  font-weight: 100 !important;
+  padding: 1rem;
+  color: Black;
+  text-align: center;
+  margin-bottom: 1rem;
+  font-family: "Philosopher", sans-serif !important;
+  @media screen and (max-width: 320px) {
+    margin-bottom: 1rem;
+  }
+  @media (min-width: 321px) and (max-width: 399px) {
+    margin-bottom: 1rem;
+  }
+  @media (min-width: 400px) and (max-width: 499px) {
+    margin-bottom: 1rem;
+  }
+  @media screen and (min-width: 1000px) {
+    font-size: 1.2rem;
+    padding: 2rem 3rem;
+  }
 
+  @media screen and (min-width: 1200px) {
+    font-size: 1.2rem;
+    padding: 2rem 8rem;
+  }
+`;
 const Container = styled.div`
   position: relative;
   display: flex;
@@ -54,6 +80,9 @@ const SkeletonContainer = styled.div`
 
 const Iframe = styled.iframe`
   width: 100%;
-  height: 100vh;
+  height: 100%;
   border: none;
+  @media screen and (max-width: 768px) {
+    height: 100%;
+  }
 `;
